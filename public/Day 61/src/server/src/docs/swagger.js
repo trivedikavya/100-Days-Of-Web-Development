126c3ecd7d7285cd7e7f9e86d545275e789b08fd
trivedikavya <kavyatrivedi931@gmail.com>
2026-01-07 21:12:58 +0530

    Docker Setup and Swagger UI has Been Added

---
+++ public/Day 61/src/server/src/docs/swagger.js
@@ -0,0 +1,39 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.setupSwagger = void 0;
+const swagger_jsdoc_1 = __importDefault(require("swagger-jsdoc"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "B2C E-commerce API",
+            version: "1.0.0",
+            description: "API documentation for the B2C E-commerce platform REST endpoints",
+        },
+        servers: [
+            {
+                url: "http://localhost:5000/api/v1",
+                description: "Development server",
+            },
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: "http",
+                    scheme: "bearer",
+                    bearerFormat: "JWT",
+                },
+            },
+        },
+    },
+    apis: ["src/modules/**/*.routes.ts"], // Scan all route files for JSDoc comments
+};
+const swaggerSpec = (0, swagger_jsdoc_1.default)(options);
+const setupSwagger = (app) => {
+    app.use("/api-docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerSpec));
+};
+exports.setupSwagger = setupSwagger;
